1f07756a23e291cc6d4c1ae224dc77e4a3a43d80
Brian Maitner <bmaitner@usf.edu>
2026-05-07 13:42:22 -0400

    updated references

--- R/fit_density_ratio.R
+++ R/fit_density_ratio.R
@@ -1,7 +1,7 @@
 #' @name fit_density_ratio
 #' @title Fit density-ratio distribution models in a plug-and-play framework.
 #' @description This function fits density-ratio species distribution models for
-#' the specified density-ratio method \insertCite{Drake2018-ha}{S4DM}.
+#' the specified density-ratio method \insertCite{Drake2018-ha,Maitner2026-bc}{S4DM}.
 #' @param presence dataframe of covariates at presence points
 #' @param background Dataframe of covariates at background points
 #' @param method Character. See "notes" for options.
